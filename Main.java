9562fea691e20b6e10054f828b58c15ea587897a
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-07-28 01:09:16 -0400

    [feat] complete Stage 4
    Update converter to now convert from one base to another. Decided to 'roll my own' instead of using the built-in methods.

--- Main.java
+++ Main.java
@@ -1,30 +1,48 @@
 package converter;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        long decimal = input.nextLong();
-        int  radix   = input.nextInt();
-        String number = Long.toString(decimal, radix);
-        String prefix;
-        switch (radix) {
-            case 2:
-                prefix = "0b";
-                break;
-            case 8:
-                prefix = "0";
-                break;
-            case 16:
-                prefix = "0x";
-                break;
-            default:
-                prefix = "";
-                break;
+
+        int    srcRadix = input.nextInt();
+        String srcNum   = input.next().toLowerCase();
+        int    tgtRadix = input.nextInt();
+
+        System.out.println(fromDecimal(toDecimal(srcNum, srcRadix), tgtRadix));
+    }
+
+    private static long toDecimal(String number, int radix) {
+        String digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+        long decimal = 0;
+
+        for (int i = number.length() - 1, power = 0; i >= 0; i--) {
+            int digit = digits.indexOf(number.charAt(i));
+            decimal += digit * Math.pow(radix, power);
+            power++;
         }
 
-        System.out.println(prefix + number);
+        return decimal;
+    }
+
+    private static String fromDecimal(long number, int radix) {
+        StringBuilder result = new StringBuilder();
+
+        if (radix == 1) {
+            char[] ones = new char[(int) number];
+            Arrays.fill(ones, '1');
+            result.append(ones);
+        } else {
+            String digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+            do {
+                long index = number % radix;
+                result.insert(0, digits.charAt((int) index));
+                number /= radix;
+            } while (number != 0);
+        }
+        return String.valueOf(result);
     }
 }
